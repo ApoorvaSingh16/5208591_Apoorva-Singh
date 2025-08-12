c2db1ddf85c1b11189abf41e63af0894214d4e4e
Apoorva Singh <apoorvasingh027@gmail.com>
2025-08-13 03:08:13 +0530

    added the code of pangram

---
+++ Hackerrank/Week 1/Pangrams.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'pangrams' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+string pangrams(string s) {
+    int arr[26]={0};
+    
+    for(int i =0; i<s.size();i++)
+    {
+        if (s[i])
+        {
+            arr[tolower(s[i]) - 'a']++;
+        }
+    }
+    for(int i= 0;i<26;i++)
+    {
+        if(arr[i]==0)
+        {
+            return "not pangram";
+        }
+    }
+    return "pangram";
+    
+    
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string result = pangrams(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
