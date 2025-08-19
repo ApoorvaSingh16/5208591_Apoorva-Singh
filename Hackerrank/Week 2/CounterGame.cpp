05801e79294a1c7182c2c73714b73296364c2d4f
Apoorva Singh <apoorvasingh027@gmail.com>
2025-08-20 02:17:55 +0530

    added the code of CounterGame

---
+++ Hackerrank/Week 2/CounterGame.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'counterGame' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts LONG_INTEGER n as parameter.
+ */
+
+string counterGame(long n) {
+    int count =0;
+    while(n>1){
+    if((n&(n-1))==0)
+    {
+    n =n/2;
+    }
+    else
+    
+     {
+        long p =1L <<(int)log2(n);
+        n -=p;
+        
+        
+    }
+    count ++;
+    }
+    return(count%2 ==0)? "Richard" : "Louise";
+    
+    
+    
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        long n = stol(ltrim(rtrim(n_temp)));
+
+        string result = counterGame(n);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
